391f642c4b00c257fef03981f138a63393fcc30e
mycodemylife <>
2020-08-27 16:29:23 +0800

    update from gridea: 2020-08-27 16:29:23

--- media/scripts/index.js
+++ media/scripts/index.js
@@ -1,1 +1,29 @@
-console.log('Hello Gridea')
+$(function(){
+	var a_idx = 0;
+	$("body").click(function(e) {
+        var a = new Array("❤富强❤","❤民主❤","❤文明❤","❤和谐❤","❤自由❤","❤平等❤","❤公正❤","❤法治❤","❤爱国❤","❤敬业❤","❤诚信❤","❤友善❤");
+		a_idx = (a_idx ) % a.length;
+        var $i = $("<span></span>").text(a[a_idx]);
+        
+        var x = e.pageX,
+        y = e.pageY;
+        $i.css({
+            "z-index": 99999999999999999999999,
+            "top": y - 20,
+            "left": x,
+            "position": "absolute",
+            "font-weight": "bold",
+            "color": "rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")"
+        });
+        $("body").append($i);
+        $i.animate({
+            "top": y - 180,
+            "opacity": 0
+        },
+        1500,
+        function() {
+            $i.remove();
+        });
+		a_idx++;
+    });
+})
